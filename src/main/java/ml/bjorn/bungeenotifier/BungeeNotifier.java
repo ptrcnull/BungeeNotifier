0d206a05016b0b31827b8ba56c8a3ccd7096b20c
Bjornskjald <github@bjorn.ml>
2019-08-06 06:54:59 +0200

    wip: Get current/target server from event

--- src/main/java/ml/bjorn/bungeenotifier/BungeeNotifier.java
+++ src/main/java/ml/bjorn/bungeenotifier/BungeeNotifier.java
@@ -1,6 +1,9 @@
 package ml.bjorn.bungeenotifier;
 
+import net.md_5.bungee.api.event.ServerConnectEvent;
+import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.event.EventHandler;
 
 public final class BungeeNotifier extends Plugin {
 
@@ -12,5 +15,11 @@ public final class BungeeNotifier extends Plugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+    class ConnectListener implements Listener {
+        @EventHandler
+        public void onPlayerJoin(ServerConnectEvent event) {
+            event.getTarget().getName();
+        }
     }
 }
